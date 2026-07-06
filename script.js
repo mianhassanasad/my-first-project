81c6489f0a6f3eefcd39220ead43b1275d3131a5
Mian Hassan <mianhassandev@gmail.com>
2026-07-06 14:58:06 +0500

    Message

---
+++ script.js
@@ -0,0 +1,9 @@
+let fullName = "Mian Hassan";
+let profession = "Web Developer";
+let experience = 3;
+let married = true;
+
+console.log(`Name: ${fullName}`);
+console.log(`Profession: ${profession}`);
+console.log(`Experience: ${experience} years`);
+console.log(`Married: ${married}`);
